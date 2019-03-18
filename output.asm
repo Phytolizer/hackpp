e97ef1c9f81ffe6b3dd59191d3e369fdcbee3d6d
Kyle Coffey <mariorules24@gmail.com>
2019-03-18 15:41:29 -0500

    Initial commit

---
+++ output.asm
@@ -0,0 +1,20 @@
+(myFunction)
+@0
+D = M
+@15
+M = D
+@ret
+A = M
+0;JMP
+
+@RET1
+D = A
+@ret
+M = D
+@1
+D = A
+@0
+M = D
+@myFunction
+0;JMP
+(RET1)
---
+++ result.asm
@@ -0,0 +1,16 @@
+(myFunction)
+@0
+D = M
+@15
+M = D
+@ret
+A = M
+0;JMP
+
+@RET1
+D = A
+@ret
+M = D
+@myFunction
+0;JMP
+(RET1)
